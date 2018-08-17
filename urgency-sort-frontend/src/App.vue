70348d706916740ad181a6faa60845fbfaeb038c
lunaroyster <oneinaquadrillion@live.com>
2018-08-17 00:15:06 +0000

    Added vue-cli autogenerate

---
+++ urgency-sort-frontend/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <Nav/>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Nav from './components/Nav';
+
+export default {
+  name: 'App',
+  components: {
+    Nav,
+  },
+};
+</script>
+
+<style lang='scss'>
+@import '~@/styles/custom-bootstrap';
+</style>
---
+++ urgency-sort-frontend/src/components/Nav.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav>
+
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Nav',
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
