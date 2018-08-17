cf2c22b489021f355aa178f019db736e4409660c
lunaroyster <oneinaquadrillion@live.com>
2018-08-17 19:46:42 +0000

    Urgency score (avg) generators

--- urgency-sort-firebase/functions/index.js
+++ urgency-sort-firebase/functions/index.js
@@ -22,6 +22,27 @@ exports.makeReports = functions.https.onRequest(async (req, res)=> {
    res.status(201).send();
 });
 
+const generateUrgencies = async ()=> {
+  let snapshot = await db.collection('requests').get();
+  let urgencies = {};
+  snapshot.forEach(doc=> {
+    let reports = Object.values(doc.data().reports);
+    for(let i in reports) {
+      if(reports[i]==-1) reports[i] = 0; //Ignoring spam reports
+    }
+    let score = reports.reduce((a,b)=>a+b)/reports.length; //Using averages. Switch with Wilson Score when there's enough data.
+    urgencies[doc.id] = score;
+  });
+  await db.collection('urgencyCaches').doc('cache').set({time: Date.now(), urgencies});
+  return urgencies;
+};
+
 exports.getUrgencies = functions.https.onRequest(async (req, res)=> {
-    
+  cors(req, res, () => {});
+  let cacheDoc = await db.collection('urgencyCaches').doc('cache').get();
+  let urgencies = cacheDoc.data().urgencies;
+  if((Date.now() - (cacheDoc.data().time||0)) > 3600000) {
+    urgencies = await generateUrgencies();
+  }
+  res.json(urgencies);
 });
