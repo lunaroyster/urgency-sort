df8587214c61073f7542f98176d45dc1d6e2d646
lunaroyster <oneinaquadrillion@live.com>
2018-08-17 03:01:54 +0000

    Installed cors

--- urgency-sort-firebase/functions/index.js
+++ urgency-sort-firebase/functions/index.js
@@ -3,7 +3,7 @@ const functions = require('firebase-functions');
 
 admin.initializeApp(functions.config().firebase);
 
-const cors = require('cors');
+const cors = require('cors')({origin: true});
 
 const db = admin.firestore();
 db.settings({
@@ -11,6 +11,7 @@ db.settings({
 });
 
 exports.makeReports = functions.https.onRequest(async (req, res)=> {
+   cors(req, res, () => {});
    let reports = req.body.reports;
    let reportIDs = Object.keys(reports);
    for (let reportID of reportIDs) {
