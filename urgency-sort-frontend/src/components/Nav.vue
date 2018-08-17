83776d124d7511fb920125739577f45d61167fc0
lunaroyster <oneinaquadrillion@live.com>
2018-08-17 11:56:07 +0000

    Added nav

--- urgency-sort-frontend/src/components/Nav.vue
+++ urgency-sort-frontend/src/components/Nav.vue
@@ -1,6 +1,18 @@
 <template>
-  <nav>
-
+  <nav class="navbar fixed-top navbar-expand-lg navbar-light">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item d-flex" style="align-items: center; font-size: 0.5em">
+          <a class="nav-link" href="https://keralarescue.in">Kerala Rescue Main Site</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://github.com/lunaroyster/urgency-sort"><i class="fab fa-github"></i></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://twitter.com/itsarnavb"><i class="fab fa-twitter"></i></a>
+        </li>
+      </ul>
+    </div>
   </nav>
 </template>
 
@@ -14,5 +26,7 @@ export default {
 </script>
 
 <style scoped>
-
+nav {
+  font-size: 1.5em;
+}
 </style>
