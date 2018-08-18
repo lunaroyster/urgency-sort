c242760d4f1e12191837b6f668e973d25fc866f7
lunaroyster <oneinaquadrillion@live.com>
2018-08-18 13:43:42 +0000

    Router changes

--- urgency-sort-frontend/src/router/index.js
+++ urgency-sort-frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/components/Home';
+import Urgencies from '@/components/Urgencies';
 
 Vue.use(Router);
 
@@ -11,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home,
+    },
+    {
+      path: '/urgent',
+      name: 'Urgencies',
+      component: Urgencies,
     },
     {
       path: '*',
