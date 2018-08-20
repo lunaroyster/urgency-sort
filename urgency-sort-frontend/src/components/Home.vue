201b0397550472d85adefa872fe8a19575be7778
lunaroyster <oneinaquadrillion@live.com>
2018-08-20 21:45:58 +0000

    Used cards

--- urgency-sort-frontend/src/components/Home.vue
+++ urgency-sort-frontend/src/components/Home.vue
@@ -2,56 +2,61 @@
   <div id="home">
     <div class="container main">
       <div class="py-4" v-if="request">
-        <h5 class="text-muted">Request <a :href="getRequestUrl(request.id)" target="_blank">#{{request.id}}</a></h5>
-        <div class="text-muted">{{getFrom(request.dateadded)}} for <span v-if="request.is_request_for_others">someone else</span><span v-else>self</span></div>
         <div class="row">
           <div class="col-12 col-6-md">
-            <table class="table">
-              <thead>
-                <tr>
-                  <th scope="col-3">Need</th>
-                  <th scope="col-9">Description</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr v-if="request.needfood">
-                  <th scope="row"><i class="fas fa-cookie mr-2"></i>Food</th>
-                  <td>{{request.detailfood}}</td>
-                </tr>
-                <tr v-if="request.needwater">
-                  <th scope="row"><i class="fas fa-tint mr-2"></i>Water</th>
-                  <td>{{request.detailwater}}</td>
-                </tr>
-                <tr v-if="request.needcloth">
-                  <th scope="row"><i class="fas fa-tshirt mr-2"></i>Clothing</th>
-                  <td>{{request.detailcloth}}</td>
-                </tr>
-                <tr v-if="request.needkit_util">
-                  <th scope="row"><i class="fas fa-utensils mr-2"></i>Kitchen Utilities</th>
-                  <td>{{request.detailkit_util}}</td>
-                </tr>
-                <tr v-if="request.needtoilet">
-                  <th scope="row"><i class="fas fa-shower mr-2"></i>Toiletries</th>
-                  <td>{{request.detailtoilet}}</td>
-                </tr>
-                <tr v-if="request.needmed">
-                  <th scope="row"><i class="fas fa-briefcase-medical mr-2"></i>Medications</th>
-                  <td>{{request.detailmed}}</td>
-                </tr>
-                <tr v-if="request.needrescue">
-                  <th scope="row"><i class="fas fa-people-carry mr-2"></i>Rescue</th>
-                  <td>{{request.detailrescue}}</td>
-                </tr>
-                <tr v-if="request.needothers">
-                  <th scope="row"><i class="fas fa-info mr-2"></i>Other</th>
-                  <td>{{request.needothers}}</td>
-                </tr>
-                <tr v-if="request.requestee_phone">
-                  <th scope="row"><i class="fas fa-phone mr-2"></i>Phone</th>
-                  <td>{{request.requestee_phone}}</td>
-                </tr>
-              </tbody>
-            </table>
+            <div class="card border-0">
+              <div class="card-body">
+                <h5 class="text-muted mb-4">Request <a :href="getRequestUrl(request.id)" target="_blank">#{{request.id}}</a></h5>
+                <table class="table">
+                  <thead>
+                    <tr>
+                      <th scope="col">Need</th>
+                      <th scope="col">Description</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr v-if="request.needfood">
+                      <th scope="row"><i class="fas fa-cookie mr-2"></i>Food</th>
+                      <td>{{request.detailfood}}</td>
+                    </tr>
+                    <tr v-if="request.needwater">
+                      <th scope="row"><i class="fas fa-tint mr-2"></i>Water</th>
+                      <td>{{request.detailwater}}</td>
+                    </tr>
+                    <tr v-if="request.needcloth">
+                      <th scope="row"><i class="fas fa-tshirt mr-2"></i>Clothing</th>
+                      <td>{{request.detailcloth}}</td>
+                    </tr>
+                    <tr v-if="request.needkit_util">
+                      <th scope="row"><i class="fas fa-utensils mr-2"></i>Kitchen Utilities</th>
+                      <td>{{request.detailkit_util}}</td>
+                    </tr>
+                    <tr v-if="request.needtoilet">
+                      <th scope="row"><i class="fas fa-shower mr-2"></i>Toiletries</th>
+                      <td>{{request.detailtoilet}}</td>
+                    </tr>
+                    <tr v-if="request.needmed">
+                      <th scope="row"><i class="fas fa-briefcase-medical mr-2"></i>Medications</th>
+                      <td>{{request.detailmed}}</td>
+                    </tr>
+                    <tr v-if="request.needrescue">
+                      <th scope="row"><i class="fas fa-people-carry mr-2"></i>Rescue</th>
+                      <td>{{request.detailrescue}}</td>
+                    </tr>
+                    <tr v-if="request.needothers">
+                      <th scope="row"><i class="fas fa-info mr-2"></i>Other</th>
+                      <td>{{request.needothers}}</td>
+                    </tr>
+                  </tbody>
+                </table>
+                <div class="d-flex" style="align-items: center;">
+                  <div class="pr-2">
+                    <small class="text-muted">{{getFrom(request.dateadded)}} for <span v-if="request.is_request_for_others">someone else</span><span v-else>self</span></small>
+                  </div>
+                  <a style="margin-left: auto" class="btn btn-outline-dark" :href="getTel(request.requestee_phone)"><i class="fas fa-phone mr-2"></i>{{request.requestee_phone}}</a>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -95,6 +100,7 @@ export default {
       return moment(timestamp).from();
     },
     getRequestUrl: id=>`https://keralarescue.in/request_details/${id}/`,
+    getTel: number=>`tel:${number}`,
   },
   computed: {
     downloadMBs() {
