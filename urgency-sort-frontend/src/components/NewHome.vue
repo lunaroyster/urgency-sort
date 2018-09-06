2084059232559740845ffd40238cfc018fcffb55
lunaroyster <oneinaquadrillion@live.com>
2018-09-06 07:05:33 +0000

    Improvements

--- urgency-sort-frontend/src/components/NewHome.vue
+++ urgency-sort-frontend/src/components/NewHome.vue
@@ -2,9 +2,9 @@
   <div id="home">
     <div class="container" style="margin-top: 2em;">
       <h1 class="display-4">What's this?</h1>
-      <p>In August 2018, floods decimated the state of Kerala. Requests for help began to arrive quick over at keralarescue.in, which was the official portal for disaster response.</p>
-      <p>I discovered that there wasn't a good way to find urgent requests as they arrived, so I built a platform that made use of the power of crowds to determine the urgency of requests. This site let volunteers rate the urgency of requests on a scale from 0 to 3. </p>
-      <p>We were able to classify about 6800 requests, and made the data available to the rescue authorities.</p>
+      <p>In August 2018, floods decimated the state of Kerala. Requests for help began to arrive quick at keralarescue.in, which was the official portal for disaster response.</p>
+      <p>I discovered that there wasn't a good way to find urgent requests since there were so many. So I built a platform that made use of the power of crowds to determine the urgency of requests. This site let volunteers rate the urgency of requests on a scale from 0 to 3.</p>
+      <p>Hundreds of volunteers began using this tool as a triage platform. We were able to classify about 6800 requests, and made the data available to the rescue authorities. You can read the full story here:</p>
       <a class="linkButton btn d-flex align-items-center my-2" href="https://medium.freecodecamp.org/how-i-used-crowdsourcing-to-help-kerala-floods-rescue-operations-12dc97c9a24b" target="_blank">
         <div class="p-2 mr-2">
           <i class="fab fa-medium-m" style="font-size: 3em;"></i>
